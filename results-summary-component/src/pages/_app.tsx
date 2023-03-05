423be524e84b2fd222af61ef8425fc98eca32afc
Morksvart <arkantos405@gmail.com>
2023-03-05 03:49:11 +0100

    final

--- results-summary-component/src/pages/_app.tsx
+++ results-summary-component/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
+
+import { Hanken_Grotesk } from "next/font/google";
 import type { AppProps } from 'next/app'
 
+const HankenGrotesk = Hanken_Grotesk({ subsets: ['latin'], style: ['normal'], weight: ['500', '700', '800'] })
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<main className={HankenGrotesk.className}><Component {...pageProps} /></main>)
 }
--- results-summary-component/src/pages/index.tsx
+++ results-summary-component/src/pages/index.tsx
@@ -4,20 +4,16 @@ import data from "../../data.json"
 import { useState } from "react";
 
 const colorMap = {
-    "Reaction-bg": "bg-red-50",
-    "Memory-bg": "bg-yellow-50",
-    "Verbal-bg": "bg-green-50",
-    "Visual-bg": "bg-blue-50",
-    "Reaction-text": "text-red-400",
-    "Memory-text": "text-yellow-400",
-    "Verbal-text": "text-green-400",
-    "Visual-text": "text-blue-400",
+    "Reaction-bg": "red",
+    "Memory-bg": "yellow",
+    "Verbal-bg": "teal",
+    "Visual-bg": "blue"
 } as const
 
 export default function Index() {
     const [items] = useState(data);
-    
-    
+
+
     return (
         <>
             <Head>
@@ -26,36 +22,42 @@ export default function Index() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="bg-white">
-                <article>
-                    <div className="p-4 bg-gradient-to-t from-blue-700 to-violet-500">
-                        <h3>Your result</h3>
-                        <p>
-                            <span>76</span> of 100
+            <main className="bg-white min-h-screen w-screen md:flex md:justify-center md:items-center
+              md:mx-auto">
+                <article className="md:min-h-full md:rounded-3xl  min-h-screen shadow-xl flex flex-col md:flex-row g-3 ">
+                    <div className="md:flex-initial md:w-5/12  text-white rounded-b-3xl flex gap-2  md:rounded-3xl flex-col items-center justify-center p-5 md:items-center bg-div">
+                        <h3 className="flex-initial">Your result</h3>
+                        <p className="flex flex-col items-center text-center justify-center gap-1 rounded-full w-32 h-32 bg-circle shadow-lg">
+                            <span className="text-5xl ">76</span>
+                            <span className="text-xs text-gray-300 ">of&ensp;100</span>
                         </p>
-                        <h2>Great</h2>
-                        <p>Your scored higher than 65% of the people who have takenthese tests</p>
+                        <h2 className="font-extrabold text-xl">Great</h2>
+                        <p className="p-1 text-center text-sm text-gray-200">Your scored higher than 65% of the people who have taken these tests</p>
                     </div>
-                    <div className="text-black p-4">
-                        <h2>Summary</h2>
-                        <div>
+                    <div className="md:flex-1 flex-grow  flex flex-col gap-4 text-black font-semibold   p-4 justify-between">
+
+                        <div className="flex  flex-col gap-4 md:flex-none ">
+                            <h2 className="flex-initial font-bold text-gray-800">Summary</h2>
                             {items.map((item) => (
                                 <div key={item.category}
-                                    className={`${colorMap[item.category+"-bg" as keyof typeof colorMap]} flex`}>
-
-                                    <Image
-                                        alt={item.category}
-                                        src={item.icon}
-                                        width={22}
-                                        height={22}
-                                        className="flex-none">
-                                    </Image>
-                                    <h3 className={`flex-1 ${colorMap[item.category+"-text" as keyof typeof colorMap]} font-bold`}>{item.category}</h3>
-                                    <p>{item.score} <span className="text-gray-500">/ 100</span></p>
+                                    className={`gap-3 p-3 rounded-lg ${colorMap[item.category + "-bg" as keyof typeof colorMap]} flex`}>
+                                    <div className="self-center">
+                                        <Image
+                                            alt={item.category}
+                                            src={item.icon}
+                                            width={22}
+                                            height={22}
+                                            className="flex-none">
+                                        </Image>
+                                    </div>
+                                    <h3 className={`flex-1`}>{item.category}</h3>
+                                    <p className="text-slate-900">{item.score} <span className="text-gray-500">/ 100</span></p>
                                 </div>
                             ))}
                         </div>
-                        <button className="rounded-3xl h-14 w-24 bg-blue-900 text-white">Continue</button>
+                        <button onClick={function (e) { alert('ciao') }} type="submit"
+                            className="select-none rounded-3xl p-4 bg-slate-800 hover:bg-blue-700 active:bg-blue-900 text-white">Continue</button>
+                    
                     </div>
                 </article>
             </main>
