00e51094ca89854f93c8fb594301bde1bff973eb
Morksvart <arkantos405@gmail.com>
2023-03-11 23:47:11 +0100

    fixini

--- results-summary-component/src/pages/_app.tsx
+++ results-summary-component/src/pages/_app.tsx
@@ -5,5 +5,5 @@ import type { AppProps } from 'next/app'
 
 const HankenGrotesk = Hanken_Grotesk({ subsets: ['latin'], style: ['normal'], weight: ['500', '700', '800'] })
 export default function App({ Component, pageProps }: AppProps) {
-  return (<main className={HankenGrotesk.className}><Component {...pageProps} /></main>)
+  return (<div className={HankenGrotesk.className}><Component {...pageProps} /></div>)
 }
